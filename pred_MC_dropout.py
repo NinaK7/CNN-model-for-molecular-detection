fa396b0fb75f22c3b5d6429d47993421049672ea
NinaK7 <116814638+NinaK7@users.noreply.github.com>
2025-08-24 12:56:30 +0200

    Update pred_MC_dropout.py

--- pred_MC_dropout.py
+++ pred_MC_dropout.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
-import pandas as pd
-
 import sys
-sys.path.insert(0,'CIANNA/src/build/lib.linux-x86_64-cpython-311') #your path to CIANNA
+sys.path.insert(0,'CIANNA/src/build/lib.linux-x86_64-cpython-311') # your path to CIANNA version
 import CIANNA as cnn
 
-def i_ar(int_list):
+############################################################################
+##              Data reading and normalizaton
+############################################################################
+
+def i_ar(int_list): # number to integer
 	return np.array(int_list, dtype="int")
 
-def f_ar(float_list):
+def f_ar(float_list): # number to float 32
 	return np.array(float_list, dtype="float32")
 
-def transfo(data_set, target) : # normalization
+def transfo(data_set) : # normalization
 	shape = data_set.shape
 
     if len(shape)==1 : 
@@ -36,47 +37,46 @@ def transfo(data_set, target) : # normalization
 	# polynomial normalization
 	data_3chan[:, 2*shape[1]:] = data_norm**3
 
-	return data_3chan, target
+	return data_3chan
 
 channels = 35000
-freq = np.linspace(80, 115, channels)
 
+# list of molecules that are detected and the classification follows this order
 molecules = ['aGg\'-(CH2OH)2','C2H3CN','C2H5CN','C2H5OH','C3H7CN','CH3CCH','CH3CHO','CH3CN','CH3COCH3', 
              'CH3NH2', 'CH3OCH3','CH3OCHO','CH3OH','CH2NH','gGg\'-(CH2OH)2','HCCCN', 'HC(O)NH2','t-HCOOH', 
              'H2CS', 'NH2CN']
-
 nb_mol = len(molecules)
 
-nb_data = 1
-data = np.zeros((nb_data, channels))
-
-mask_raw = np.load('./mask.npy')
-mask = np.ones(channels) - mask_raw[:-1]
+# loading of the spectrum to be tested
 path_test = './SPECTRA2TEST/'
-name = ['spec1', 'spec2']
-data = np.nan_to_num(np.load(path_test + "spectrum_to_test.npy"))* mask
+nb_data = 1
+data = np.nan_to_num(np.load(path_test + "spectrum_to_test.npy")) # needs to be reshaped to (1, 35000) if it is not yet the case
+target = np.zeros((nb_data, nb_mol)) # targets to zero
 
-target = np.zeros((nb_data, nb_mol))
-data_norm, target =  transfo(data, target)
-nb_test = len(target)
+mask = np.load('./mask.npy') # mask loading
+data_norm, target =  transfo(data*mask) # normalization of the spectrum
 
+nb_data_MC = 100 # number of realizations to be done
+target_MC = np.zeros((nb_data_MC, nb_mol)) # targets to zero
+data_MC = np.tile(data_norm, (nb_data_MC, 1)) # production of an array with nb_data_MC times the same spectrum
 
-nb_data_MC = 100
-target_MC = np.zeros((nb_data_MC, nb_mol))
-data_MC = np.tile(data_norm[spec], (nb_data_MC, 1))
-nb_test = len(target_MC)
+############################################################################
+##               CIANNA network construction and use
+############################################################################
 
+# initialisation of the backbone
 cnn.init(in_dim=i_ar([channels]), in_nb_ch=3, out_dim=nb_mol, bias=0.1, 
         b_size=32, comp_meth="C_CUDA", dynamic_load=1, mixed_precision="FP32C_FP32A", inference_only=1, no_logo=1)
+
+# loading of the data to test by CIANNA
 cnn.create_dataset("TEST", size=i_ar(nb_data_MC), input=f_ar(data_MC), target=f_ar(target_MC))
 
 path_cnn = './net_save/'
-load_epoch = 99
-cnn.load(path_cnn + "net0_s%04d.dat"%load_epoch,load_epoch, bin=1)
-cnn.forward(drop_mode='MC_MODEL',no_error = 1, repeat=1, saving=2, silent=1)
+load_iteration = 99
 
-pred = np.fromfile("./fwd_res/net0_%04d.dat"%(load_epoch), dtype='float32')
+cnn.load(path_cnn + "net0_s%04d.dat"%load_iteration, load_iteration, bin=1) # weights loading
+cnn.forward(drop_mode='MC_MODEL',no_error = 1, repeat=1, saving=2, silent=1) # Forward propagation 
+
+pred = np.fromfile("./fwd_res/net0_%04d.dat"%(load_iteration), dtype='float32')
 pred = np.reshape(pred,(nb_data_MC,nb_mol+1))
-print(pred)
-
 
